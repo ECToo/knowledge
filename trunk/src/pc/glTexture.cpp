900dffadbf5c92649663ef216ad2539fbcbb0c73
abra185 <abra185@7deb688e-cc55-0410-97da-5f43d21d503b>
2009-07-09 08:37:30 +0000

    -Correct names, prevent conflicts

--- trunk/src/pc/glTexture.cpp
+++ trunk/src/pc/glTexture.cpp
@@ -15,150 +15,268 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef __WII__
-
 #include "texture.h"
 #include "root.h"
 #include "logger.h"
 
 namespace k {
-
-void materialStage::draw()
+			
+texture::texture(const std::string& filename, int flags)
 {
-	bool notFound = true;
-	for (int i = 0; i < K_MAX_STAGE_TEXTURES; i++)
+	// Check if file exists
+	FILE* fileExist = fopen(filename.c_str(), "rb");
+
+	if (!fileExist)
 	{
-		if (mTextures[i])
+		S_LOG_INFO("Failed to open file " + filename + ", it doesnt exist or you dont have permissions to access it.");
+		return;
+	}
+
+	fclose(fileExist);
+
+	FREE_IMAGE_FORMAT imgFormat = FreeImage_GetFileType(filename.c_str(), 0);
+	if (imgFormat == FIF_UNKNOWN)
+	{
+		S_LOG_INFO("FreeImage was unable to load texture " + filename);
+		return;
+	}
+
+	int imgFlags = 0;
+	if (imgFormat == FIF_JPEG)
+		imgFlags = JPEG_ACCURATE;
+
+	FIBITMAP* image = FreeImage_Load(imgFormat, filename.c_str(), imgFlags);
+	if (!image)
+	{
+		S_LOG_INFO("FreeImage was unable to load texture " + filename);
+		return;
+	}
+
+	mWidth = FreeImage_GetWidth(image);
+	mHeight = FreeImage_GetHeight(image);
+	mFormat = TEX_RGBA;
+	mFlags = flags;
+
+	unsigned int bpp = FreeImage_GetBPP(image);
+
+	if (!mWidth || !mHeight)
+	{
+		std::stringstream tempStr;
+		tempStr << "Invalid texture size (" << filename;
+		tempStr << ", " << mWidth << "x" << mHeight << ")";
+
+		S_LOG_INFO(tempStr.str());
+		return;
+	}
+
+	if (bpp != 32 && bpp != 24)
+	{
+		FIBITMAP* tmp = FreeImage_ConvertTo32Bits(image);
+		FreeImage_Unload(image);
+		image = tmp;
+	}
+
+	mRawData = NULL;
+	try 
+	{
+		mRawData = new unsigned char[mWidth * mHeight * 4];
+	}
+
+	catch (...)
+	{
+		S_LOG_INFO("Could not allocate memory for texture (" + filename + ") data.");
+		FreeImage_Unload(image);
+		return;
+	}
+
+	// Copy Pixel data
+	unsigned char* copyImgData = (unsigned char*)mRawData;
+	for (int j = (mHeight - 1); j >= 0; j--)
+	{
+		for (unsigned int i = 0; i < mWidth; i++)
 		{
-			notFound = false;
-			break;
+			RGBQUAD pixelColor;
+			FreeImage_GetPixelColor(image, i, j, &pixelColor);
+
+			copyImgData[2] = pixelColor.rgbRed;
+			copyImgData[1] = pixelColor.rgbGreen;
+			copyImgData[0] = pixelColor.rgbBlue;
+			copyImgData[3] = pixelColor.rgbReserved;
+			copyImgData += 4;
 		}
 	}
 
-	if (notFound)
+	FreeImage_Unload(image);
+
+	try
+	{
+		mPointer = new platformTexturePointer;
+	}
+
+	catch (...)
+	{
+		S_LOG_INFO("Failed to allocate GLuint texture for " + filename);
+		delete [] mRawData;
 		return;
-
-	renderSystem* rs = root::getSingleton().getRenderSystem();
-	rs->bindTexture(getTexture(0)->getPointer(), mIndex);
-
-	switch (mTexEnv)
-	{
-		default:
-		case TEXENV_REPLACE:
-			glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
-			break;
-
-		case TEXENV_MODULATE:
-			glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
-			break;
-
-		case TEXENV_BLEND:
-			glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_BLEND);
-			break;
-
-		case TEXENV_DECAL:
-			glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
-			break;
-
-		case TEXENV_ADD:
-			glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_ADD);
-			break;
 	}
+		
+	glGenTextures(1, mPointer);
+	glBindTexture(GL_TEXTURE_2D, *mPointer);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, mWidth, mHeight, 0, GL_BGRA, GL_UNSIGNED_BYTE, mRawData);
 
-	if (mBlendSrc || mBlendDst)
-	{
-		rs->setBlend(true);
-		rs->setBlendMode(mBlendSrc, mBlendDst);
-	}
+	// Wrapping S
+	if (mFlags & (1 << FLAG_CLAMP_EDGE_S))
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	else
+	if (mFlags & (1 << FLAG_CLAMP_S))
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+	else
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+
+	// Wrapping T
+	if (mFlags & (1 << FLAG_CLAMP_EDGE_T))
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	else
+	if (mFlags & (1 << FLAG_CLAMP_T))
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+	else
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	// Wrapping R
+	if (mFlags & (1 << FLAG_CLAMP_EDGE_R))
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+	else
+	if (mFlags & (1 << FLAG_CLAMP_R))
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_R, GL_CLAMP);
+	else
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_R, GL_REPEAT);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	delete [] mRawData;
+	mRawData = NULL;
+}
+			
+texture::texture(void* data, unsigned int w, unsigned int h, int flags, int format)
+{
+	mWidth = w;
+	mHeight = h;
+	mFormat = format;
+	mFlags = flags;
+
+	try
 	{
-		rs->setBlend(false);
+		mPointer = new platformTexturePointer;
 	}
 
-	switch (mCoordType)
+	catch (...)
 	{
+		S_LOG_INFO("Failed to allocate GLuint texture for dynamic data.");
+		return;
+	}
+
+	int realFormat = GL_RGBA;
+	switch (mFormat)
+	{
+		case TEX_RGB:
+			realFormat = GL_RGB;
+			break;
+		case TEX_RGBA:
+			realFormat = GL_RGBA;
+			break;
+		case TEX_BGR:
+			realFormat = GL_BGR;
+			break;
+		case TEX_BGRA:
+			realFormat = GL_BGRA;
+			break;
 		default:
-		case TEXCOORD_UV:
-			// Dont touch texture matrix
-			if (!mRotate && !mScroll.x && !mScroll.y && !mScale.x && !mScale.y)
-				break;
-
-			glMatrixMode(GL_TEXTURE);
-			glLoadIdentity();
-
-			if (mRotate)
-			{
-				glRotatef(mAngle, 0, 0, 1);
-				mAngle += mRotate;
-			}
-
-			if (mScroll.x || mScroll.y)
-			{
-				glTranslatef(mScrolled.x, mScrolled.y, 0);
-				mScrolled.x += mScroll.x;
-				mScrolled.y += mScroll.y;
-			}
-
-			if (mScale.x || mScale.y)
-			{
-				glScalef(mScale.x, mScale.y, 1);
-			}
-			break;
-		case TEXCOORD_SPHERE:
-			glTexGeni(GL_S, GL_TEXTURE_GEN_MODE, GL_SPHERE_MAP);
-			glTexGeni(GL_T, GL_TEXTURE_GEN_MODE, GL_SPHERE_MAP);
-			glTexGeni(GL_R, GL_TEXTURE_GEN_MODE, GL_SPHERE_MAP);
-			glEnable(GL_TEXTURE_GEN_S);
-			glEnable(GL_TEXTURE_GEN_T);
-			glEnable(GL_TEXTURE_GEN_R);
-			break;
+			S_LOG_INFO("Texture Format not supported by openGL");
+			return;
 	}
+		
+	glGenTextures(1, mPointer);
+	glBindTexture(GL_TEXTURE_2D, *mPointer);
+	glTexImage2D(GL_TEXTURE_2D, 0, realFormat, mWidth, mHeight, 0, GL_BGRA, GL_UNSIGNED_BYTE, data);
+
+	// Wrapping S
+	if (mFlags & (1 << FLAG_CLAMP_EDGE_S))
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	else
+	if (mFlags & (1 << FLAG_CLAMP_S))
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+	else
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+
+	// Wrapping T
+	if (mFlags & (1 << FLAG_CLAMP_EDGE_T))
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	else
+	if (mFlags & (1 << FLAG_CLAMP_T))
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+	else
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	// Wrapping R
+	if (mFlags & (1 << FLAG_CLAMP_EDGE_R))
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+	else
+	if (mFlags & (1 << FLAG_CLAMP_R))
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_R, GL_CLAMP);
+	else
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_R, GL_REPEAT);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	glBindTexture(GL_TEXTURE_2D, 0);
+	mRawData = NULL;
 }
 
-void materialStage::finish()
+texture::~texture()
 {
-	bool notFound = true;
-	for (int i = 0; i < K_MAX_STAGE_TEXTURES; i++)
-	{
-		if (mTextures[i])
-		{
-			notFound = false;
-			break;
-		}
-	}
+	if (mPointer)
+		delete mPointer;
+}
 
-	if (notFound)
+void texture::setFlags(unsigned int flags)
+{
+	if (!mPointer)
 		return;
 
-	renderSystem* rs = root::getSingleton().getRenderSystem();
+	mFlags = flags;
+	glBindTexture(GL_TEXTURE_2D, *mPointer);
 
-	// unBind texture
-	rs->unBindTexture(mIndex);
+	// Wrapping S
+	if (mFlags & (1 << FLAG_CLAMP_EDGE_S))
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	else
+	if (mFlags & (1 << FLAG_CLAMP_S))
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+	else
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 
-	// Reset params
-	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+	// Wrapping T
+	if (mFlags & (1 << FLAG_CLAMP_EDGE_T))
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	else
+	if (mFlags & (1 << FLAG_CLAMP_T))
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+	else
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-	// Reset Blending
-	if (mBlendSrc || mBlendDst)
-		rs->setBlend(false);
-
-	switch (mCoordType)
-	{
-		default:
-		case TEXCOORD_NONE:
-		case TEXCOORD_UV:
-			glMatrixMode(GL_TEXTURE);
-			glLoadIdentity();
-			break;
-
-		case TEXCOORD_SPHERE:
-			glDisable(GL_TEXTURE_GEN_S);
-			glDisable(GL_TEXTURE_GEN_T);
-			break;
-	}
+	// Wrapping R
+	if (mFlags & (1 << FLAG_CLAMP_EDGE_R))
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+	else
+	if (mFlags & (1 << FLAG_CLAMP_R))
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_R, GL_CLAMP);
+	else
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_R, GL_REPEAT);
 }
-
+			
 }
-
-#endif
 
