13feaf39d925b0b7071dd5ce22e50c717a6a1e35
abra185 <abra185@7deb688e-cc55-0410-97da-5f43d21d503b>
2009-10-12 18:44:02 +0000

    -Add xcode project (needs further testing) -Removed ode dependencies for codeblocks and makefiles

---
+++ trunk/projects/xcode/xcode.h
@@ -0,0 +1,23 @@
+/*
+ *  xcode.h
+ *  xcode
+ *
+ *  Created by nightz on 9/14/09.
+ *  Copyright 2009 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#ifndef xcode_
+#define xcode_
+
+/* The classes below are exported */
+#pragma GCC visibility push(default)
+
+class xcode
+{
+	public:
+		void HelloWorld(const char *);
+};
+
+#pragma GCC visibility pop
+#endif
---
+++ trunk/projects/xcode/xcodePriv.h
@@ -0,0 +1,19 @@
+/*
+ *  xcodePriv.h
+ *  xcode
+ *
+ *  Created by nightz on 9/14/09.
+ *  Copyright 2009 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+/* The classes below are not exported */
+#pragma GCC visibility push(hidden)
+
+class xcodePriv
+{
+	public:
+		void HelloWorldPriv(const char *);
+};
+
+#pragma GCC visibility pop
